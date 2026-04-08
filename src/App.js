f234e1420eaa2e9813ef0df4195324618aade026
AlexImbacuan <alex.imbacuan@correounivalle.edu.co>
2026-04-07 22:15:18 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Bienvenido a mi app Siev</h1>
+      <p>Esta es una vista de ejemplo con React.</p>
+      <button onClick={() => alert('¡Hola desde React!')}>
+        Haz clic
+      </button>
     </div>
   );
 }
