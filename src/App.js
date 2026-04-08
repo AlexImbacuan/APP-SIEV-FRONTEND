7dfa3a962b6aa91c06a8ce6059603fb3385fd265
AlexImbacuan <alex.imbacuan@correounivalle.edu.co>
2026-04-07 22:36:47 -0500

    prueba de desplegue 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Bienvenido a mi app Siev</h1>
+      <h1>Bienvenido a mi app Siev-prueba de desplege</h1>
       <p>Esta es una vista de ejemplo con React.</p>
       <button onClick={() => alert('¡Hola desde React!')}>
         Haz clic
